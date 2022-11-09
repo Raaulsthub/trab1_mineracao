bb7ed798bca9362005ee4926ecd87de60afa97fc
Raaulsthub <rsteinmetz@inf.ufsm.br>
2022-11-09 16:09:46 -0300

    .

--- association.py
+++ association.py
@@ -9,7 +9,7 @@ print(df.head(10), end='\n\n')
 
 
 # Building the model
-frq_items = apriori(df, min_support = 0.05, use_colnames = True)
+frq_items = apriori(df, min_support = 0.1, use_colnames = True)
   
 # Collecting the inferred rules in a dataframe
 rules = association_rules(frq_items, metric ="lift", min_threshold = 1)
@@ -18,6 +18,7 @@ rules = rules.sort_values(['confidence', 'lift'], ascending =[False, False])
 index = np.arange(len(rules.index))
 rules['index'] = index
 rules.set_index('index', inplace=True)
+
 rules.to_csv("./data/rules.csv")
 
 
